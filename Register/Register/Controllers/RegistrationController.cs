7e01be97beb1974c199852ea57ebd7100f2b506f
Bepa88 <leleiko.vira@gmail.com>
2019-12-26 17:40:43 +0200

    marge

---
+++ Register/Register/Controllers/RegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using Register.Filters;
+using RegisterWeb.Models;
+using RegisterWeb.ModelsDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RegisterWeb.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        RegisterDbContext DB = new RegisterDbContext();
+
+        [HttpPost]
+        [ValidateModel]
+        [Route("api/register")]
+        public HttpResponseMessage Registration(EmailPassword login)
+        {
+                ModelsDB.Register emailPassword = new ModelsDB.Register();
+                var logins = DB.Register.Where(p => p.Email == login.Email);
+                if (logins != null)
+                {
+                    emailPassword.Email = login.Email;
+                    emailPassword.Password = login.Password;
+                    DB.Register.Add(emailPassword);
+                    DB.SaveChanges();
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                }
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+    }
+}
---
+++ Register/Register/Models/EmailPassword.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegisterWeb.Models
+{
+    public class EmailPassword
+    {
+        [EmailAddress]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 100 char")]
+        [Required(AllowEmptyStrings = false)]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [System.ComponentModel.DefaultValue("")]
+        public string Email { get; set; }
+
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 100 char")]
+        [Required(AllowEmptyStrings = false)]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        [System.ComponentModel.DefaultValue("")]
+        public string Password { get; set; }
+    }
+}
